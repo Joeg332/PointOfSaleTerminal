7273a4efaf60b3fdf52e220faab1e335346de9ea
Joseph Giardina <joeg332@gmail.com>
2021-11-23 16:55:37 -0500

    creating Controller API endpoints and calling backend

--- src/main/java/com/percent/PointOfSaleTerminal/api/ItemController.java
+++ src/main/java/com/percent/PointOfSaleTerminal/api/ItemController.java
@@ -1,10 +1,12 @@
 package com.percent.PointOfSaleTerminal.api;
 
 import com.percent.PointOfSaleTerminal.handler.ItemHandler;
+import com.percent.PointOfSaleTerminal.model.Item;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.util.Map;
 
 @EnableSwagger2
 @RequestMapping(value = "/v1", produces = "application/json")
@@ -18,7 +20,29 @@ public class ItemController {
         this.itemHandler = itemHandler;
     }
 
+    @GetMapping("/item")
+    public Map<String, Item> getItems(){
+        return itemHandler.getItems();
+    }
 
+    @GetMapping("/item/{itemCode}")
+    public Item getItem(@PathVariable String itemCode){
+        return itemHandler.getItem(itemCode);
+    }
 
+    @PostMapping("/item")
+    public Item createItem(@RequestBody Item item){
+        return itemHandler.createItem(item);
+    }
+
+    @PutMapping("/item/{itemCode}")
+    public Item updateItem(@RequestBody Item item,
+                           @PathVariable String itemCode){
+        return itemHandler.updateItem(item, itemCode);
+    }
+    @DeleteMapping ("/item/{itemCode}")
+    public Item deleteItem(@PathVariable String itemCode){
+        return itemHandler.DeleteItem(itemCode);
+    }
 
 }
--- src/main/java/com/percent/PointOfSaleTerminal/api/TransactionController.java
+++ src/main/java/com/percent/PointOfSaleTerminal/api/TransactionController.java
@@ -1,10 +1,12 @@
 package com.percent.PointOfSaleTerminal.api;
 
 import com.percent.PointOfSaleTerminal.handler.TransactionHandler;
+import com.percent.PointOfSaleTerminal.model.Transaction;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.util.UUID;
 
 @EnableSwagger2
 @RequestMapping(value = "/v1", produces = "application/json")
@@ -18,5 +20,28 @@ public class TransactionController {
         this.transactionHandler = transactionHandler;
     }
 
+    @GetMapping("/transaction/{transactionId}")
+    public Transaction getTransaction(@PathVariable UUID transactionId){
+        return transactionHandler.getTransaction(transactionId);
+    }
 
+    @PostMapping("/transaction/scan/{itemCode}")
+    public Transaction createTransaction(@PathVariable String itemCode){
+        return transactionHandler.createTransactionAndScan(itemCode);
+    }
+
+    @PutMapping("/transaction/{transactionId}/scan/{itemCode}")
+    public Transaction updateTransaction(@PathVariable UUID transactionId, @PathVariable String itemCode){
+        return transactionHandler.scanAndUpdateTransaction(transactionId, itemCode);
+    }
+
+    @DeleteMapping("/transaction/{transactionId}/transactionItem/{transactionItemId}")
+    public Transaction removeItemFromTransaction(@PathVariable UUID transactionId, @PathVariable UUID transactionItemId){
+        return transactionHandler.removeItemFromTransaction(transactionId, transactionItemId);
+    }
+
+    @GetMapping("/transaction/{transactionId}/calculateTotal")
+    public Transaction removeItemFromTransaction(@PathVariable UUID transactionId){
+        return transactionHandler.calculateTotal(transactionId);
+    }
 }
